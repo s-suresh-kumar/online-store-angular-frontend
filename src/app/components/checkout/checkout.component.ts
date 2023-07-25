95217086feb42a4d5f30cb2ecec67718cac6520b
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-25 09:30:59 -0400

    add more fields lastname and email to form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -12,13 +12,17 @@ export class CheckoutComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.checkoutFormGroup = this.formBuilder.group({});
+    this.checkoutFormGroup = this.formBuilder.group({
     customer: this.formBuilder.group( {
       firstName: [''],
       lastName: [''],
       email: ['']
     })
+    });
   }
   
-
+onSubmit() {
+  console.log("Handling the  submit button");
+  console.log(this.checkoutFormGroup.get('customer')?.value);
+}
 }
