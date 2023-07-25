bd063498b1d008b40840e1be4a4ea568ec5409c8
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-25 14:42:52 -0400

    make state drop down work based on chosen country

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
 import { DeepDiscountShoppingFormService } from 'src/app/services/deep-discount-shopping-form.service';
 
 @Component({
@@ -18,64 +19,67 @@ export class CheckoutComponent implements OnInit {
   creditCardMonths: number[] = [];
   countries: Country[] = [];
 
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder,
-              private deepDiscountShoppingFormService: DeepDiscountShoppingFormService) {}
+    private deepDiscountShoppingFormService: DeepDiscountShoppingFormService) { }
 
   ngOnInit(): void {
-   
+
 
     this.checkoutFormGroup = this.formBuilder.group({
-   
-    customer: this.formBuilder.group( {
-      firstName: [''],
-      lastName: [''],
-      email: ['']
-    }),
-    shippingAddress: this.formBuilder.group( {
-      street: [''],
-      city: [''],
-      state: [''],
-      country: [''],
-      zipCode: ['']
-    }),
-    billingAddress: this.formBuilder.group( {
-      street: [''],
-      city: [''],
-      state: [''],
-      country: [''],
-      zipCode: ['']
-    }),
-    creditCard: this.formBuilder.group( {
-      cardType: [''],
-      nameOnCard: [''],
-      cardNumber: [''],
-      securityCode: [''],
-      expirationMonth: [''],
-      expirationYear: ['']
 
-    })
-  });
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
 
-      // populate credit card months
+      })
+    });
 
-      const startMonth: number = new Date().getMonth() + 1;
-      console.log("startMonth: " + startMonth);
-  
-      this.deepDiscountShoppingFormService.getCreditCardMonths(startMonth).subscribe(
-        data => {
-          console.log("Retrieved credit card months: " + JSON.stringify(data));
-          this.creditCardMonths = data;
-        }
-      );
-  
-      // populate credit card years
-  
-      this.deepDiscountShoppingFormService.getCreditCardYears().subscribe(
-        data => {
-          console.log("Retrieved credit card years: " + JSON.stringify(data));
-          this.creditCardYears = data;
-        }
-      );
+    // populate credit card months
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.deepDiscountShoppingFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    // populate credit card years
+
+    this.deepDiscountShoppingFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
 
     // populate countries
 
@@ -92,13 +96,13 @@ export class CheckoutComponent implements OnInit {
     this.shippingAddressChecked = !this.shippingAddressChecked;
     if (this.shippingAddressChecked) {
       this.checkoutFormGroup.controls['billingAddress']
-      .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
-}
-else {
-      this.checkoutFormGroup.controls['billingAddress'].reset();
-}
+        .setValue(this.checkoutFormGroup.controls['shippingAddress'].value);
     }
-  
+    else {
+      this.checkoutFormGroup.controls['billingAddress'].reset();
+    }
+  }
+
   // copyShippingAddressToBillingAddress(event: Event) {
   //   if (event.target instanceof HTMLElement) {
   //   if(event.target.checked) {
@@ -110,35 +114,64 @@ else {
   //   }
   // }
 
-onSubmit() {
-  console.log("Handling the  submit button");
-  console.log(this.checkoutFormGroup.get('customer')?.value);
-  console.log("the email address is " + this.checkoutFormGroup.get('customer')?.value.email);
-}
-
-handleMonthsAndYears() {
-
-  const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
-
-  const currentYear: number = new Date().getFullYear();
-  const selectedYear: number  = Number(creditCardFormGroup?.value.expirationYear);
-
-  // if the current year equals the selected year, then start with the current month
-
-  let startMonth: number;
-
-  if (currentYear === selectedYear) {
-    startMonth = new Date().getMonth() + 1;
-  }
-  else {
-    startMonth = 1;
+  onSubmit() {
+    console.log("Handling the  submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log("the email address is " + this.checkoutFormGroup.get('customer')?.value.email);
   }
 
-  this.deepDiscountShoppingFormService.getCreditCardMonths(startMonth).subscribe(
-    data => {
-      console.log("Retrieved credit card months: " + JSON.stringify(data));
-      this.creditCardMonths = data;
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
     }
-  );
-}
+    else {
+      startMonth = 1;
+    }
+
+    this.deepDiscountShoppingFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.deepDiscountShoppingFormService.getStates(countryCode).subscribe(
+      data => {
+
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        }
+        else {
+          this.billingAddressStates = data;
+        }
+
+        // select first item by default
+        if (formGroup != undefined && formGroup != null ) {
+        formGroup.get('state')!.setValue(data[0]);
+        }
+      }
+    );
+  }
+
 }
