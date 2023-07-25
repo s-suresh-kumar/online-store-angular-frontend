7e7d62ff65e503040ed2d1f6344d49c34db11b2a
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-25 13:57:31 -0400

    create Country and State common modules and basic attributes,  adjust DeepDiscountShoppingFormService class

--- src/app/services/deep-discount-shopping-form.service.ts
+++ src/app/services/deep-discount-shopping-form.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
@@ -5,7 +6,11 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class DeepDiscountShoppingFormService {
-  constructor() { }
+
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) { }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
 
