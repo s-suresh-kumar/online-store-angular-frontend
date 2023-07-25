b0600ab1950e69ee3c39d25864cd9cc36c1d860d
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-25 12:28:56 -0400

    update html and ts of checkout   component for months of  year

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -48,7 +48,9 @@ export class CheckoutComponent implements OnInit {
       nameOnCard: [''],
       cardNumber: [''],
       securityCode: [''],
-      expirationMonth: ['']
+      expirationMonth: [''],
+      expirationYear: ['']
+
     })
   });
 
@@ -100,5 +102,31 @@ onSubmit() {
   console.log("Handling the  submit button");
   console.log(this.checkoutFormGroup.get('customer')?.value);
   console.log("the email address is " + this.checkoutFormGroup.get('customer')?.value.email);
+}
+
+handleMonthsAndYears() {
+
+  const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+  const currentYear: number = new Date().getFullYear();
+  const selectedYear: number  = Number(creditCardFormGroup?.value.expirationYear);
+
+  // if the current year equals the selected year, then start with the current month
+
+  let startMonth: number;
+
+  if (currentYear === selectedYear) {
+    startMonth = new Date().getMonth() + 1;
+  }
+  else {
+    startMonth = 1;
+  }
+
+  this.deepDiscountShoppingFormService.getCreditCardMonths(startMonth).subscribe(
+    data => {
+      console.log("Retrieved credit card months: " + JSON.stringify(data));
+      this.creditCardMonths = data;
+    }
+  );
 }
 }
