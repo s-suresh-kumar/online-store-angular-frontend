31eda547e4ea916612c88c8b35579ab399cf31f0
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-31 15:47:07 -0400

    secure  members only page/component added

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default  {
 
   oidc: {
-    clientId: '4wvtwmpbwrIfhNYWodIEWsU39XaZu5Bc',
-    issuer: 'https://dev-xogp5bu73r4ou1if.us.auth0.com/oauth2/default',
+    clientId: '0oaakcxbrctqmBWJM5d7',
+    issuer: 'https://dev-66574892.okta.com/oauth2/default',
     redirectUri: 'http://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
 }
