449f8b60c20df4d24aa9465ea435ac2ed2dc3e68
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-31 16:29:37 -0400

    fix browser refresh using session/local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -14,7 +14,21 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  // storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
+
+  constructor() {
+
+    // read data from storage
+    let data = JSON.parse(this.storage.getItem('cartItems')!);
+
+    if (data != null) {
+      this.cartItems = data;
+
+      // compute totals based on the data that is read from storage
+      this.computeCartTotals();
+    }
+  }
 
   addToCart(theCartItem: CartItem) {
 
@@ -26,12 +40,12 @@ export class CartService {
       // find the  item in the cart based on the itm id
 
       existingCartItem = this.cartItems.find(tempCartItem => tempCartItem.id === theCartItem.id);
-      
+
       // check to see if  we found the item
       alreadyExistsInCart = (existingCartItem !== undefined);
     }
 
-    if (existingCartItem != undefined ) {
+    if (existingCartItem != undefined) {
 
       // increment  the quantity
       existingCartItem.quantity++;
@@ -58,7 +72,15 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
   }
+
+  persistCartItems() {
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
+  }
+
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
     console.log('Contents of the cart');
     for (let tempCartItem of this.cartItems) {
@@ -85,7 +107,7 @@ export class CartService {
   remove(theCartItem: CartItem) {
 
     // get index of item in the array
-    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
 
     // if found, remove the item from the array at the given index
     if (itemIndex > -1) {
@@ -96,4 +118,3 @@ export class CartService {
   }
 
 }
-  
